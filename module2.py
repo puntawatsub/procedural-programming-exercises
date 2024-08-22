3edf133f8b49fe9ae6e705da8d85c9872bfbedc1
PuntawatSub <57529099+puntawatsub@users.noreply.github.com>
2024-08-22 13:45:56 +0300

    Update module2.py

--- module2.py
+++ module2.py
@@ -6,12 +6,22 @@ print("Hello,", name)
 
 radius = float(input("radius of circle: "))
 area = math.pi * (radius ** 2)
-print("Area of circle: " + str(area))
+print("Area of circle: " + str("{:.2f}".format(area)))
 
 length = float(input("length of rectangle: "))
 width = float(input("width of rectangle: "))
 perimeter = (length * 2) + (width * 2)
 print("Perimeter of rectangle: " + str(perimeter))
+
+num1 = int(input("first number: "))
+num2 = int(input("second number: "))
+num3 = int(input("third number: "))
+
+sum = num1 + num2 + num3
+average = sum / 3.0
+print("The sum is:", sum)
+print("The average is: " + str(average))
+print("The product is:", num1 * num2 * num3);
 
 talents = float(input(f"Enter talents: \n"))
 pounds = float(input(f"Enter pounds: \n")) + (talents * 20)
@@ -22,7 +32,7 @@ kilograms = int((grams - (grams % 1000)) / 1000)
 grams_left = grams - (kilograms * 1000)
 
 print("The weight in modern units:")
-print(str(kilograms), "kilograms and", str(grams_left), "grams.")
+print(str(kilograms), "kilograms and", str("{:.2f}".format(grams_left)), "grams.")
 
 print(f"\n")
 
